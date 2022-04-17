a807b4244450fa560e42c87a86921da176544b7b
madmopuse <madmopuse1991@mail.ru>
2022-04-17 14:43:22 +0300

    17.04.2022 class work 5

--- src/lesson5.java
+++ src/lesson5.java
@@ -9,5 +9,22 @@
 public class lesson5 {
     public static void main(String[] args) {
 
+        Employee employee1 = new
+                Employee("Slava", 100000, "QA Engineer", 34);
+
+        Employee employee2 = new
+                Employee("Leonid", 150000, "middle QA Engineer", 36);
+        //employee2.salary = 15000; //не санкционированный доступ к данным
+
+        System.out.println(employee1.getName() + " " + employee2.getName());
+        System.out.println(employee1.getBonus());
+        System.out.println(employee2.getBonus());
+
     }
+
+    /*public static int getBonus(int salary) {
+        return (int) (salary * 1.5); //(((double) salary) * 1.5);
+    }*/
+
+
 }
