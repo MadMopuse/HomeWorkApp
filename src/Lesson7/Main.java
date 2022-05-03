1c713a8f20655dda5f08d3e9feeae522a455d371
madmopuse <madmopuse1991@mail.ru>
2022-05-03 11:51:17 +0300

    03.05.2022 classwork7

---
+++ src/Lesson7/Main.java
@@ -0,0 +1,17 @@
+package Lesson7;
+
+/**
+ * Java 1. Lesson 7
+ *
+ * @author MadMopuse
+ * @version 03.05.2022
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+
