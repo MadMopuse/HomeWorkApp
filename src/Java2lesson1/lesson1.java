2483699900a3149b85e428cd6b638ee9b0d532c3
madmopuse <madmopuse1991@mail.ru>
2022-05-10 16:15:29 +0300

    !

---
+++ src/Java2lesson1/lesson1.java
@@ -0,0 +1,20 @@
+package Java2lesson1;
+
+/**
+ * Java 2. lesson1
+ *
+ * @author MadMopuse
+ * @version 12.05.2022 lesson1
+ *
+ */
+
+public class lesson1 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
