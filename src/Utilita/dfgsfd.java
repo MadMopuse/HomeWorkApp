bc6eb7511a027648c5a221dc5ff65f47cd784dab
madmopuse <madmopuse1991@mail.ru>
2022-08-06 16:35:21 +0300

    Java 2. HomeLesson1

---
+++ src/Utilita/dfgsfd.java
@@ -0,0 +1,23 @@
+package Utilita;
+
+public class dfgsfd {
+    public static void main(String[] args) {
+        /*byte a = 127;
+        a++;
+        System.out.println(a);
+
+
+        String a = "java";
+        a.toUpperCase();
+        System.out.println(a);
+
+         */
+
+        
+
+
+        for (int i=10; i>0; i++) {}
+    }
+
+
+}
