907d4e4500ddbe5047b6b71857a141d1d42e4703
madmopuse <madmopuse1991@mail.ru>
2022-08-28 22:37:46 +0300

    Java 2. HomeLesson3 complited tasks

---
+++ src/Java2lesson3/Fruits.java
@@ -0,0 +1,19 @@
+package Java2lesson3;
+
+public class Fruits {
+
+    public double weightFruits;
+
+    public double getWeightFruits() {
+        return weightFruits;
+    }
+
+    public void setWeightFruits(double weightFruits) {
+        this.weightFruits = weightFruits;
+    }
+
+    public Fruits(double weight) {
+        this.weightFruits = weight;
+    }
+
+}
---
+++ src/Java2lesson3/lesson3.java
@@ -0,0 +1,59 @@
+package Java2lesson3;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class lesson3 {
+    /**
+     * Java 2. HomeLesson3
+     *
+     * @author MadMopuse
+     * @version 22.08.2022 HomeLesson3
+     */
+
+    public static void main(String[] args) {
+        //Задача 2
+        Box box1 = new Box();
+        for (int i = 0; i < 10; i++ ) {
+            box1.addFruits(new Apple());
+        }
+        box1.addFruits(new Orange());
+
+        System.out.println("weight of box1 = " + box1.getWeightBox());
+
+        Box box2 = new Box();
+        for (int i = 0; i < 6; i++){
+            box2.addFruits(new Orange());
+        }
+
+        if (box1.compare(box2))
+            System.out.println("weight of box1 = weight box2");
+        else System.out.println("weight of box1 != weight box2");
+
+        Box box3 = new Box();
+
+        box1.outAllFruits(box3);
+        System.out.println("weight of box1 = " + box1.getWeightBox() +"\n" + "weight of box3 = " + box3.getWeightBox());
+
+        String[] values = {"1", "2", "s"};
+        changeValue(values, 0, 2);
+
+        Integer[] values2 = {10, 20, 30};
+        changeValue(values2, 0, 2);
+
+    }
+
+// Задача 1
+    private static <T> void changeValue(T[] values, int i, int i1) {
+        T temp = values[i];
+        values[i] = values[i1];
+        values[i1] = temp;
+        System.out.println(Arrays.toString(values));
+    }
+
+
+
+
+
+}
