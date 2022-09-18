4102f1fa5b6624d715a7461b561fccf87da4f972
MadMopuse <madmopuse1991@mail.ru>
2022-09-18 21:43:57 +0300

    HomeLesson6 start

---
+++ src/Java2/lesson6/lesson6.java
@@ -0,0 +1,20 @@
+package Java2.lesson6;
+
+import Java2.lesson5.AppData1;
+
+import java.util.Arrays;
+
+/**
+ * Java 2. HomeLesson6
+ *
+ * @author MadMopuse
+ * @version 18.09.2022 HomeLesson5
+ */
+
+public class lesson6 {
+
+    public static void main(String[] args) {
+        AppData1 data = new AppData1();
+
+    }
+}
